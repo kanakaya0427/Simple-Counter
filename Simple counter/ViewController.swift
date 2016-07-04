434c291bf8262ccc6e5a3fb78aa5720e2b1210fa
NakayaKana <kanakaya@me.com>
2016-07-04 13:18:46 +0900

    Initial Commit

---
+++ Simple counter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple counter
+//
+//  Created by NakayaKana on 2016/07/04.
+//  Copyright © 2016年 NakayaKana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
